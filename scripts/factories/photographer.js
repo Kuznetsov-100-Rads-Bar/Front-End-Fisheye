3be6cb140b6b7f4d5c58310418325276a1348b7d
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-04-22 02:47:53 +0200

    Prendre en main la base du code

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,13 +1,16 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+
+
+        const article = document.createElement('article');
+
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
