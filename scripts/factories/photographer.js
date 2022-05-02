00697bbebaa288c33dd7c8c017c1a73d6415704a
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-05-02 04:16:47 +0200

    Mise à jour du fichier

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,15 +6,41 @@ function photographerFactory(data) {
     function getUserCardDOM() {
 
 
+        const link = document.createElement('a');
+        link.setAttribute("href", document.location + 'photographer.html?id=' + id);
         const article = document.createElement('article');
 
         const img = document.createElement('img');
         img.setAttribute("src", picture);
         const h2 = document.createElement('h2');
+        h2.style.marginBottom = "4px";
         h2.textContent = name;
+
+        const location = document.createElement('p');
+        location.textContent = `${country}, ${city}`;
+        location.style.color = '#901C1C';
+        location.style.fontSize = '16px';
+        location.style.margin = '0'
+
+        const slogan = document.createElement('p');
+        slogan.textContent = `${tagline}`;
+        slogan.style.color = '#000000';
+        slogan.style.fontSize = '13px';
+        slogan.style.margin = '0'
+
+        const photographerPrice = document.createElement('p');
+        photographerPrice.textContent = `${price}€/jour`
+        photographerPrice.style.color = '#757575';
+        photographerPrice.style.fontSize = '12px';
+        photographerPrice.style.margin = '0'
+
+        link.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(location);
+        article.appendChild(slogan);
+        article.appendChild(photographerPrice);
+        return (link);
     }
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,80 @@
-//Mettre le code JavaScript lié à la page photographer.html
+/* It creates a new instance of a photographer profile, which is then displayed on the page. */
+class PhotographerProfile {
+    /**
+     * It's a constructor that creates a new object with the given parameters and returns it.
+     * @param name - string
+     * @param location - "Paris"
+     * @param slogan - "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl
+     * eget congue congue, nisl nisi porta nisl, eget tincidunt nisl nisi eget n
+     * @param image - the name of the image file (e.g. "image.jpg")
+     */
+    constructor(name, location, slogan, image) {
+        this.name = name;
+        this.location = location;
+        this.slogan = slogan;
+        this.image = image;
+
+        const photographHeader = document.querySelector(".photograph-header");
+
+        const photographInformations = document.createElement("div");
+        photographInformations.classList.add('photograph-informations');
+
+        const photographName = document.createElement('h3');
+        photographName.classList.add('photograph-name');
+        photographName.textContent = this.name;
+
+        const photographLocation = document.createElement("p");
+        photographLocation.classList.add('photograph-location');
+        photographLocation.textContent = this.location;
+
+        const photographSlogan = document.createElement('p');
+        photographSlogan.classList.add('photograph-slogan');
+        photographSlogan.textContent = this.slogan;
+
+        const photographPicture = document.createElement('img');
+        photographPicture.classList.add('photograph-picture');
+        photographPicture.setAttribute('src', `./assets/photographers/${this.image}`);
+        photographPicture.setAttribute('alt', `Photo de profil de ${this.name}`);
+
+        photographHeader.prepend(photographInformations);
+        photographHeader.appendChild(photographPicture);
+        photographInformations.appendChild(photographName);
+        photographName.after(photographLocation);
+        photographLocation.after(photographSlogan);
+    }
+}
+
+/**
+ * It fetches the photographer's data from the JSON file and returns it.
+ * @returns The photographer object.
+ */
+const fetchPhotographer = async () => {
+    const photographerId = parseInt(new URLSearchParams(window.location.search).get('id'));
+
+    const fetchPhotographer = await fetch('./data/photographers.json').then((data) => data.json());
+
+    const photographer = fetchPhotographer.photographers.find((photographer) => photographer.id === photographerId);
+
+    if (!photographerId || !Number(photographerId) || !photographer) {
+        return window.location.assign('../');
+    }
+    return photographer;
+}
+
+const init = async () => {
+    const photographer = await fetchPhotographer();
+
+    const photographerProfile = new PhotographerProfile(photographer.name, `${photographer.city}, ${photographer.country}`, photographer.tagline, photographer.portrait);
+
+    // setInterval(() => {
+    //     photographerProfile.edit("Photographe bien")
+    //     setInterval(() => {
+    //         photographerProfile.edit(photographer.name)
+    //     }, 2000);
+    // }, 2000);
+
+
+    // console.log(photographer)
+}
+
+init();
