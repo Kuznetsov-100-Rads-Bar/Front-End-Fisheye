55346144433967041fc093d28098a9cad28d55ab
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-06-28 22:58:01 +0200

    last comments

--- scripts/models/photographerProfile.model.js
+++ scripts/models/photographerProfile.model.js
@@ -1,15 +1,16 @@
 /* eslint-disable max-len */
-/* It creates a new instance of a photographer profile, which is then displayed on the page. */
+/* Il crée une nouvelle instance d'un profil de photographe, qui est ensuite affiché sur la page. */
+/* Il exporte la classe à utiliser dans un autre fichier. */
 export default class {
   /**
-     * It's a constructor that creates a new object with the given parameters and returns it.
+     * C'est un constructeur qui crée un nouvel objet avec les paramètres donnés et le renvoie.
      * @param name - string
      * @param location - "Paris"
      * @param slogan - "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl
      * eget congue congue, nisl nisi porta nisl, eget tincidunt nisl nisi eget n
      * @param image - the name of the image file (e.g. "image.jpg")
      */
-  /* It's a constructor that creates a new object with the given parameters and returns it. */
+  /* C'est un constructeur qui crée un nouvel objet avec les paramètres donnés et le renvoie. */
   constructor(name, location, slogan, image) {
     this.name = name;
     this.location = location;
@@ -17,73 +18,90 @@ export default class {
     this.image = image;
   }
 
+  /* C'est une méthode qui affiche le profil du photographe sur la page. */
   display = () => {
     const formHeaderTitle = document.getElementById('formHeaderTitle');
     formHeaderTitle.innerHTML = `${formHeaderTitle.textContent} <br/>${this.name}`;
     formHeaderTitle.style.textAlign = 'left';
-    /* It's a variable that stores the element with the class "photograph-header" in the DOM. */
+
+    /* C'est une variable qui stocke l'élément avec la classe "photograph-header" dans le DOM. */
     const photographHeader = document.querySelector('.photograph-header');
 
-    /* It creates a new div element and stores it in the variable photographInformations. */
+    /* Il crée un nouvel élément div et le stocke dans la variable photographInformations. */
     const photographInformations = document.createElement('div');
-    /* It adds the class "photograph-informations" to the element stored in the variable
-        photographInformations. */
+    /* Il ajoute la classe "photograph-informations" à l'élément stocké dans la variable
+        photographieInformations. */
+
     photographInformations.classList.add('photograph-informations');
 
-    /* It creates a new h3 element and stores it in the variable photographName. */
+    /* Il crée un nouvel élément h3 et le stocke dans la variable photographName. */
     const photographName = document.createElement('h3');
-    /* It adds the class "photograph-name" to the element stored in the variable photographName. */
+
+    /* Il ajoute la classe "photograph-name" à l'élément stocké dans la variable photographName. */
     photographName.classList.add('photograph-name');
-    /* It's setting the text content of the element stored in the variable photographName to the
-        value of the property name of the object stored in the variable this. */
+
+    /* Il définit le contenu textuel de l'élément stocké dans la variable photographName sur le
+        valeur du nom de propriété de l'objet stocké dans la variable this. */
     photographName.textContent = this.name;
 
-    /* It creates a new p element and stores it in the variable photographLocation. */
+    /* Il crée un nouvel élément p et le stocke dans la variable photographLocation. */
     const photographLocation = document.createElement('p');
-    /* It adds the class "photograph-location" to the element stored in the variable
-        photographLocation. */
+
+    /* Il ajoute la classe "photograph-location" à l'élément stocké dans la variable
+        photographieEmplacement. */
     photographLocation.classList.add('photograph-location');
-    /* It's setting the text content of the element stored in the variable photographLocation to the
-        value of the property location of the object stored in the variable this. */
+
+    /* Il définit le contenu textuel de l'élément stocké dans la variable photographLocation sur le
+        valeur de la propriété location de l'objet stocké dans la variable this. */
     photographLocation.textContent = this.location;
 
-    /* It creates a new p element and stores it in the variable photographSlogan. */
+    /* Il crée un nouvel élément p et le stocke dans la variable photographSlogan. */
     const photographSlogan = document.createElement('p');
-    /* It adds the class "photograph-slogan" to the element stored in the variable
-        photographSlogan. */
+
+    /* Il ajoute la classe "photograph-slogan" à l'élément stocké dans la variable
+        photographieSlogan. */
     photographSlogan.classList.add('photograph-slogan');
-    /* It's setting the text content of the element stored in the variable photographSlogan to the
-        value of the property slogan of the object stored in the variable this. */
+
+    /* Il définit le contenu textuel de l'élément stocké dans la variable photographSlogan sur
+        valeur de la propriété slogan de l'objet stocké dans la variable this. */
     photographSlogan.textContent = this.slogan;
 
-    /* It creates a new img element and stores it in the variable photographPicture. */
+    /* Il crée un nouvel élément img et le stocke dans la variable photographPicture. */
     const photographPicture = document.createElement('img');
-    /* It adds the class "photograph-picture" to the element stored in the variable
-        photographPicture. */
+
+    /* Il ajoute la classe "photograph-picture" à l'élément stocké dans la variable
+        photographieImage. */
     photographPicture.classList.add('photograph-picture');
-    /* It's setting the src attribute of the element stored in the variable photographPicture to
-        the value of the property image of the object stored in the variable this. */
+
+    /* Il définit l'attribut src de l'élément stocké dans la variable photographPicture sur
+        la valeur de la propriété image de l'objet stockée dans la variable this. */
     photographPicture.setAttribute('src', `./assets/photographers/${this.image}`);
-    /* It's setting the alt attribute of the element stored in the variable photographPicture to
-        the value of the property name of the object stored in the variable this. */
+
+    /* Il définit l'attribut alt de l'élément stocké dans la variable photographPicture sur
+        la valeur du nom de propriété de l'objet stocké dans la variable this. */
     photographPicture.setAttribute('alt', `Photo de profil de ${this.name}`);
 
-    /* It's adding the element stored in the variable photographInformations as the first child of
-        the element stored in the variable photographHeader. */
+    /* C'est ajouter l'élément stocké dans la variable photographInformations comme premier enfant de
+        l'élément stocké dans la variable photographHeader. */
     photographHeader.prepend(photographInformations);
-    /* It's adding the element stored in the variable photographPicture as the last child of the
-        element stored in the variable photographHeader. */
+
+    /* C'est ajouter l'élément stocké dans la variable photographPicture comme dernier enfant de la
+        élément stocké dans la variable photographHeader. */
     photographHeader.appendChild(photographPicture);
-    /* It's adding the element stored in the variable photographName as the last child of the
-        element stored in the variable photographInformations. */
+
+    /* Il s'agit d'ajouter l'élément stocké dans la variable photographName comme dernier enfant de la
+        élément stocké dans la variable photographInformations. */
     photographInformations.appendChild(photographName);
-    /* It's adding the element stored in the variable photographLocation after the element stored in
-        the variable photographName. */
+
+    /* C'est ajouter l'élément stocké dans la variable photographLocation après l'élément stocké dans
+        la variable photoName. */
     photographName.after(photographLocation);
-    /* It's adding the element stored in the variable photographSlogan after the element stored in
-        the variable photographLocation. */
+
+    /* C'est ajouter l'élément stocké dans la variable photographSlogan après l'élément stocké dans
+        la variable photographieEmplacement. */
     photographLocation.after(photographSlogan);
 
+    /* Il renvoie l'élément stocké dans la variable photographHeader. */
     return photographHeader;
   };
 }
